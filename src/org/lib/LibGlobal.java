6fc3918ed678a18b2f6ee7c6efff3f904b33de58
ajith1197 <ajithanirudh1197@gmail.com>
2021-08-24 11:46:54 +0530

    Get driver done

--- src/org/lib/LibGlobal.java
+++ src/org/lib/LibGlobal.java
@@ -1,5 +1,10 @@
 package org.lib;
 
 public class LibGlobal {
-
+	public static void getDriver() {
+		System.out.println("getdriver");
+			}
+			public static void main(String[] args) {
+				getDriver();
+			}
 }
